4060ce7063a807271e9dbd92496a75da510f30f8
msutt <msutt@tlu.ee>
2025-09-17 23:34:35 +0300

    feat(counter): add buttons to increase/decrease count
    Allow the counter to be increased or decreased in multiple steps (1, 25, 50, 100).

--- 2025-09-11/my-app/src/components/Counter.tsx
+++ 2025-09-11/my-app/src/components/Counter.tsx
@@ -4,7 +4,7 @@ import "../App.css"
 function Counter() {
   const [count, setCount] = useState(0)
 
-  function increaseCounter(number: number) {
+  function changeCounter(number: number) {
     setCount(count => count + number)
   }
 
@@ -12,7 +12,15 @@ function Counter() {
     <>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => increaseCounter(10)}>count is {count}</button>
+        <button onClick={() => changeCounter(1)}>+1</button>
+        <button onClick={() => changeCounter(25)}>+25</button>
+        <button onClick={() => changeCounter(50)}>+50</button>
+        <button onClick={() => changeCounter(100)}>+100</button>
+        <div>count is {count}</div>
+        <button onClick={() => changeCounter(-1)}>-1</button>
+        <button onClick={() => changeCounter(-25)}>-25</button>
+        <button onClick={() => changeCounter(-50)}>-50</button>
+        <button onClick={() => changeCounter(-100)}>-100</button>
       </div>
     </>
   )
