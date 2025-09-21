5fa192b01303fdfdc1406916842fd7d8ef4bce00
Martin <martin@Jans-MacBook-Pro.local>
2025-09-21 23:19:54 +0200

    feat(router): setup createHashRouter with 3 child components and parent navigation

--- 2025-09-18/my-app/src/App.tsx
+++ 2025-09-18/my-app/src/App.tsx
@@ -1,12 +1,19 @@
 import { useState } from "react"
 import "./App.css"
 import Button from "@mui/material/Button"
+import { Link, Outlet } from "react-router-dom"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/something">Something</Link>
+        <Outlet />
+      </div>
       <div className="card">
         <button onClick={() => setCount(count => count + 1)}>
           count is {count}
--- 2025-09-18/my-app/src/main.tsx
+++ 2025-09-18/my-app/src/main.tsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react"
+import ReactDOM, { createRoot } from "react-dom/client"
+import { createHashRouter, RouterProvider } from "react-router-dom"
+import "./index.css"
+import App from "./App.tsx"
+import Home from "./components/Home.tsx"
+import About from "./components/About.tsx"
+import Something from "./components/Something.tsx"
 
-createRoot(document.getElementById('root')!).render(
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/about", element: <About /> },
+      { path: "/something", element: <Something /> },
+    ],
+  },
+])
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
