db065ddc4422d4d48005d6b9a5d3bb7bf6ade870
msutt <msutt@tlu.ee>
2025-09-30 19:45:39 +0300

    feat(components): add PropDrilling and Context examples

---
+++ 2025-09-25/martin/frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Cats from "./components/Cats";
+import PropDrilling from "./components/PropDrilling";
+import Context from "./components/Context";
+
+function App() {
+  return (
+    <>
+      <Cats />
+      <PropDrilling />
+      <Context />
+    </>
+  );
+}
+
+export default App;
